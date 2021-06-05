d50871d36d96461796b6b640810000eac8e6b65e
andrew <andrew@DDI.com>
2021-06-05 15:40:42 +0800

    test

--- ugly-code.js
+++ ugly-code.js
@@ -1,5 +1,6 @@
 var a = 'ugly-variable';
-var b = "ava    raible    with no semi  "  ;
-function name (){
-  alert();console.log("xxxxxx")
+var b = 'ava    raible    with no semi  ';
+function name() {
+    alert();
+    console.log('xxxxxx');
 }
