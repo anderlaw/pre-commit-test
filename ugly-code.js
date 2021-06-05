ddde37eb124ae452ec51a97fa3689b058f0eb2c4
andrew <andrew@DDI.com>
2021-06-05 13:36:51 +0800

    ffff

--- ugly-code.js
+++ ugly-code.js
@@ -1,4 +1,2 @@
-var a = 'ugly-variable';
-var b = 'ava    raible    with no semi';
-var a = 'ugly-variable';
-var b = 'ava    raible    with no semi';
+var a =    "ugly-variable";
+var     b = "ava    raible    with no semi"   
